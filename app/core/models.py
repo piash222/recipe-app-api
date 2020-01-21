8a6aaba8bc60f6847c66febb0e17506701d2290b
Md. Shihab Uddin <piash222@gmail.com>
2020-01-21 14:55:37 +0600

    minor changes for flake8

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+
 
 class UserManager(BaseUserManager):
 
@@ -8,8 +10,9 @@ class UserManager(BaseUserManager):
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that supports using email instead of username"""
